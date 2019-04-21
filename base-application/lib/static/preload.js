08687dc9a754e733b86bc0deb0859f473b1ef35a
FellGill <40915285+FellGill@users.noreply.github.com>
2019-04-21 20:13:15 +0200

    Create preload.js

---
+++ base-application/lib/static/preload.js
@@ -0,0 +1,7 @@
+'use strict';var _electron=require('electron');var _path=require('path');var _path2=_interopRequireDefault(_path);var _fs=require('fs');var _fs2=_interopRequireDefault(_fs);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj}}
+document.addEventListener('DOMContentLoaded',function(){injectScripts()});var INJECT_JS_PATH=_path2.default.join(__dirname,'../../','inject/inject.js');var log=require('loglevel');function setNotificationCallback(createCallback,clickCallback){var OldNotify=window.Notification;var newNotify=function newNotify(title,opt){createCallback(title,opt);var instance=new OldNotify(title,opt);instance.addEventListener('click',clickCallback);return instance};newNotify.requestPermission=OldNotify.requestPermission.bind(OldNotify);Object.defineProperty(newNotify,'permission',{get:function get(){return OldNotify.permission}});window.Notification=newNotify}
+function injectScripts(){var needToInject=_fs2.default.existsSync(INJECT_JS_PATH);if(!needToInject){return}
+require(INJECT_JS_PATH)}
+function notifyNotificationCreate(title,opt){_electron.ipcRenderer.send('notification',title,opt)}
+function notifyNotificationClick(){_electron.ipcRenderer.send('notification-click')}
+setNotificationCallback(notifyNotificationCreate,notifyNotificationClick);_electron.ipcRenderer.on('params',function(event,message){var appArgs=JSON.parse(message);log.info('nativefier.json',appArgs)});_electron.ipcRenderer.on('debug',function(event,message){log.info('debug:',message)})
